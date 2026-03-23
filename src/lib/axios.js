73649fc8692cde7669b1562fba090efb38e33b49
wanchai7 <wanchai.view47@gmail.com>
2026-03-23 15:08:33 +0700

    base-url

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000" : "/",
+    baseURL: import.meta.env.VITE_API_BASE_URL || (import.meta.env.MODE === "development" ? "http://localhost:5000" : "/"),
     withCredentials: true,
 });
